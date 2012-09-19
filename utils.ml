976e68a4c2ad3d5035236ced612a496093b9a1a7
Michael Laporte <michaell.laporte@gmail.com>
2012-09-19 14:35:09 +0200

    Ajout de commentaire de utils + suppression fonction obsolete

--- utils.ml
+++ utils.ml
@@ -1,57 +1,63 @@
 open Cmt_format
 open Typedtree
-(* Debug *)
 
+(* Ensemble de Path *)
 module PathSet = Set.Make( 
   struct
     type t = Path.t
     let compare = Pervasives.compare
   end )
 
+(* Ensemble d'Ident *)
 module IdentSet = Set.Make( 
   struct
     type t = Ident.t
     let compare = Pervasives.compare
   end )
 
+(* Structure utilisée pour les dépendances : Path * PathSet *)
 module DepMap = Map.Make( 
   struct
     let compare = Pervasives.compare
     type t = Path.t
   end )
 
+(* Outils de debug *)
 let debug_flag = ref true
 
 let debug fmt = 
   if !debug_flag then Format.printf fmt 
   else Format.ifprintf Format.err_formatter fmt
 
-
+(* Fonction pour printer un constructeur *)
 let rec print_variant = function
   | [] -> ()
   | (id,_,_,_)::xs ->
       debug "%a" Ident.print id;
       print_variant xs
 
+(* Fonction pour printer un record *)
 let rec print_rec = function
   | [] -> ()
   | (id,_,_,_,_)::xs ->
       debug "%a" Ident.print id;
       print_rec xs  
 
+(* Fonction pour printer un type_desc *)
 let rec print_type_desc ppf tv = match tv.typ_kind with
   | Ttype_abstract -> debug "abstract"
   | Ttype_variant l -> print_variant l
   | Ttype_record l -> print_rec l
     
-  
+(* Fonction pour printer un PathSet *)  
 let rec print_set_path ppf = 
   PathSet.iter (debug "%a " Printer.print_path)
 
+(* Fonction pour printer une IdentSet *)
 let rec print_ids ppf = 
   IdentSet.iter (debug "%a " Ident.print)
 
-
+(* Fonction pour printer une liste de dependances avec les index *)
 let rec print_full_graph ppf = function
   | [],[] -> ()
   | [],[(x1,x2)] -> 
@@ -63,6 +69,7 @@ let rec print_full_graph ppf = function
       debug "@[<0>%i:%a]" i Printer.print_path id;
       print_full_graph ppf (inds,xs)
 
+(* Fonction pour printer une liste de dependances *)
 let rec print_graph ppf = function
   | [] -> ()
   | [(x1,x2)] -> debug "%a -> %a \n" Printer.print_path x1  Printer.print_path x2
@@ -70,14 +77,15 @@ let rec print_graph ppf = function
       debug "%a -> %a,"  Printer.print_path x1  Printer.print_path x2;
       print_graph ppf xs
 
+(* Fonction pour printer un DepMap *)
 let rec print_graph_map ppf =
   DepMap.iter (fun k e ->
     debug "@[ %a : " Printer.print_path k;
     PathSet.iter (fun x -> debug "%a " Printer.print_path x) e;
     debug "@]"
   )
-  
 
+(* Fonction pour printer les index *)  
 let rec print_cnst ppf = function
   | [] -> ()
   | [(x1,x2)] -> debug "%i : %a \n" x1  Printer.print_path x2
@@ -85,7 +93,7 @@ let rec print_cnst ppf = function
       debug "%i : %a,"  x1  Printer.print_path x2;
       print_cnst ppf xs
 
-
+(* Fonction pour printer l'ast en prenant un .cmt *)
 let print_untype ppf fn = 
   let aux = function
     | Implementation strct -> 
@@ -94,39 +102,22 @@ let print_untype ppf fn =
   let cmt_inf = Cmt_format.read_cmt fn in
   aux cmt_inf.cmt_annots
 
-
+(* Fonction pour extraire le nom du module du nom du fichier .cmt *)
 let get_modname filename =
   let cmt_inf = Cmt_format.read_cmt filename in
   cmt_inf.cmt_modname
 
-
-let get_id_from_patexp_list l =
-  let rec aux = function
-    | Tpat_any -> Path.Pident (Ident.create "any")
-    | Tpat_var (id,loc) -> Path.Pident id
-    | Tpat_alias (p,kind,_) -> failwith "as TODO"
-    | Tpat_constant cnst -> failwith "cst TODO"
-    | Tpat_tuple pat_list -> failwith "tuple TODO"
-    (* List.fold_left (fun l i ->  *)
-    (*   List.append l (aux i.pat_desc)) [] pat_list *)
-    | Tpat_construct (path,loc,cnstor_desc,exp_list,_) -> Path.Pident (Ident.create "any")
-    | Tpat_variant (lbl,pat_option,row_desc) -> failwith "var TODO"
-    | Tpat_record (list,flag) -> failwith "rec TODO"
-    | Tpat_array pat_list -> failwith "array TODO"
-    | Tpat_or (pat1,pat2,row_desc_opt) -> failwith "or TODO"
-    | Tpat_lazy pat -> failwith "lazy TODO"
-  in (fun (p,_) -> aux p.pat_desc) (List.hd l)
-
-
+(* Fonction pour printer les graph de dependance de l'ensemble du projet *)
 let rec print_list l = 
   List.iter (fun (n,(_,d)) -> 
     debug "%s : %a\n" n print_graph d) l
 
+(* Fonction pour printer les index de l'ensemble du projet *)
 let rec print_list2 l = 
   List.iter (fun (n,(c,_)) -> 
     debug "%s : %a\n" n print_cnst c) l
 
-
+(* Fonction pour printer les DepMap de l'ensemble du projet *)
 let rec print_deps_map l = 
   List.iter (fun (n,(_,d)) -> 
     debug "%s : %a\n" n print_graph_map d) l
