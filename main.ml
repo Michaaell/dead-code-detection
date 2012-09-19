47d4908f1b92eaec01f599c92de5e2b0c7f11a0a
Michael Laporte <michaell.laporte@gmail.com>
2012-09-19 14:05:19 +0200

    Nettoyage du main

--- main.ml
+++ main.ml
@@ -15,17 +15,14 @@ let side_flag = ref false
 let parse_arg_list = Str.split (Str.regexp "[ \t]+")
 
 let rec iter_dir dirname =
-  if dirname = ""
-  then ()
-  else
-    let files = Sys.readdir dirname in
-    Array.iter (fun file ->
-      let file = Filename.concat dirname file in
-      if Sys.is_directory file then iter_dir file;
-      if Filename.check_suffix file ".cmt" then cmt_list := file::!cmt_list;
-    ) files
+  let files = Sys.readdir dirname in
+  Array.iter (fun file ->
+    let file = Filename.concat dirname file in
+    if Sys.is_directory file then iter_dir file;
+    if Filename.check_suffix file ".cmt" then cmt_list := file::!cmt_list;
+  ) files
 
-let usage = "usage: " ^ Sys.argv.(0) ^ " [cmt1 cmt2 cmt3 ...] [-elim cmt_file] [-print cmt_file] [-elim-project rep]"
+let usage = "usage: " ^ Sys.argv.(0) ^ " [-elim cmt_file] [-print cmt_file] [-elim-project rep]"
  
 let speclist = [
   ("-elim", Arg.String (fun s -> cmt_list := parse_arg_list s;elim_flag := true),
@@ -40,15 +37,7 @@ let speclist = [
 let _ =
   Arg.parse
     speclist
-    (fun x -> 
-      if Filename.check_suffix x ".cmt"
-      then 
-        begin
-          cmt_list := x::(!cmt_list);
-          test_flag := true
-        end
-      else
-        raise (Arg.Bad ("Bad argument : " ^ x)))
+    (fun x -> raise (Arg.Bad ("Bad argument : " ^ x)))
     usage;
   let fn = !filename in
   let ppf = Format.std_formatter in
@@ -73,18 +62,17 @@ let _ =
     else
       if !side_flag
       then
-        begin 
+        begin ()
           (* let mod_list = List.map (fun x -> Utils.get_modname x) !cmt_list in *)
           (* let l = List.map (fun x -> (x,Side_effect.main_side mod_list x)) !cmt_list in *)
           (* Utils.debug "%i" (List.length l) *)
-          let syst = 
-            List.map (fun x -> (Utils.get_modname x,Deps.calc x)) !cmt_list in
-          let new_syst = (* Deps.calc_inter_dep *) syst in
-          let local_deps = snd (List.split (snd (List.split new_syst))) in
-          List.iter (fun x ->
-            let new_ld = Side_effect.reverse_dep x in
-            let used = Side_effect.id_affect_by_side_effect new_ld in
-            Utils.PathSet.iter (Utils.debug "%a " Printer.print_path) used) local_deps
+          (* let syst = *)
+          (*   List.map (fun x -> (Utils.get_modname x,Deps.calc x)) !cmt_list in () *)
+          (* let local_deps = snd (List.split (snd (List.split new_syst))) in *)
+          (* List.iter (fun x -> *)
+          (*   let new_ld = Side_effect.reverse_dep x in *)
+          (*   let used = Side_effect.id_affect_by_side_effect new_ld in *)
+          (*   Utils.PathSet.iter (Utils.debug "%a " Printer.print_path) used) local_deps *)
         end
       else
       Printer.dttree ppf fn
@@ -103,3 +91,4 @@ let _ =
 
 
 
+        
